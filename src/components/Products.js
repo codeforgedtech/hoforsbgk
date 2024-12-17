c41814e52a1d5fdbc671dc38fc554dd404d95629
Christer Holm <info@codeforged.se>
2024-12-17 12:24:44 +0100

    new products

--- src/components/Products.js
+++ src/components/Products.js
@@ -75,7 +75,11 @@ const ProductPrice = styled.p`
   font-size: 1em;
   color: #666;
 `;
-
+const ProductDiscription = styled.p`
+  margin: 0;
+  font-size: 1em;
+  color: #666;
+`;
 const StyledLink = styled.a`
   display: inline-block;
   margin-top: 20px;
@@ -98,9 +102,11 @@ const Products = () => (
     <Title>Produkter</Title>
     <ProductsList>
       {[
-        { id: 1, name: 'Keps', price: 199, image: '/products/product1.jpg' },
-        { id: 2, name: 'Kylduk', price: 100, image: '/products/product2.jpg' },
-        { id: 3, name: 'Mugg', price: 175, image: '/products/product3.jpg' },
+        { id: 1, name: 'Keps', price: 199,   description: 'En stilren keps med hög komfort, perfekt för alla tillfällen.' , image: '/products/product1.jpg' },
+        { id: 2, name: 'Kylduk', price: 100, description: 'En svalkande kylduk, idealisk för varma dagar eller träning.' , image: '/products/product2.jpg' },
+        { id: 3, name: 'Mugg', price: 175,description: 'En klassisk mugg med stilren design, perfekt för kaffeälskaren.', image: '/products/product3.jpg' },
+        { id: 4, name: 'Mössa', price: 150, description: 'En varm och bekväm mössa för kyliga dagar. Uppvikt skärm ' ,image: '/products/product4.jpg' },
+        { id: 5, name: 'Mössa', price: 175,  description: 'En trendig mössa som kombinerar stil och funktionalitet.Tätare material och är varmare', image: '/products/product5.jpg' },
       ].map((product) => (
         <ProductItem key={product.id}>
           <ProductImage 
@@ -109,10 +115,11 @@ const Products = () => (
           />
           <ProductName>{product.name}</ProductName>
           <ProductPrice>{product.price} SEK</ProductPrice>
+          <ProductDiscription>{product.description}</ProductDiscription>
         </ProductItem>
       ))}
     </ProductsList>
-    Vid köp Swishas beloppet till 1234467825, skriv Keps, Tvättduk, Mugg osv , 
+    Vid köp Swishas beloppet till 1234467825, skriv Keps, Kylduk, Mugg , Mössa, osv , 
     <StyledLink href="/">Tillbaka</StyledLink>
   </FullProductsContainer>
 );
